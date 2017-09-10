572c5691dacbfa7b21f1bc06caabb116652a5387
jwwangchn <jwwangchn@outlook.com>
2017-09-10 07:02:23 +0000

    add hello world

--- Intel-Aero-Connect-LeiXu.sh
+++ Intel-Aero-Connect-LeiXu.sh
@@ -1,2 +1,4 @@
 nmcli c down hotspot
-nmcli dev wifi connect SM-G9008V password 12345678
+#nmcli dev wifi connect SM-G9008V password 12345678
+nmcli dev wifi connect HUAWEI-XXAT4C password spl207207
+
